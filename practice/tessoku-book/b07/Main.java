5ab2dd307168ce89aff5480ea1f63d8849ffaf82
miyawaki <121045410+m-miyawaki-m@users.noreply.github.com>
2024-02-17 11:58:10 +0000

    b07

--- practice/tessoku-book/b07/Main.java
+++ practice/tessoku-book/b07/Main.java
@@ -1,18 +1,36 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // 文字列 S を入力として受け取る
-        String[] strings = scanner.next().split("");
+        int T = scanner.nextInt(); // 整数 T を入力として受け取る
+        int N = scanner.nextInt(); // 整数 N を入力として受け取る
 
-        // stream を使って文字列の各文字を順に処理
-        long num = Arrays.stream(strings).filter(s -> s.equals("1")).count();
+        // D日間のイベント
+        int[] nums = new int[T];
+
+        int[] diff = new int[T+1]; // T+1 で初期化しておく（RがTと等しい場合のため）
+
+        for (int i = 0; i < N; i++) {
+            int L = scanner.nextInt();
+            int R = scanner.nextInt();
+        
+            diff[L]++;
+            if (R < T) diff[R]--;
+        }
+        
+        // 累積和を計算してnumsに格納
+        int sum = 0;
+        for (int i = 0; i < T; i++) {
+            sum += diff[i];
+            nums[i] = sum;
+        }
 
         // 最終的な結果を出力
-        System.out.println(num);
+        for(int num : nums){
+           System.out.println(num);
+        }
         scanner.close();
     }
 }
